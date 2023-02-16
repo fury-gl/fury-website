14e62d3b1ca7d50d81a163d964d51c6a88e4038c
Garyfallidis <garyfallidis@gmail.com>
2023-02-16 17:14:18 +0000

    Deploying to gh-pages from @ fury-gl/fury@2c8d4498740aa0fd3a706bb0c07a617f9e70cc70 🚀

--- v0.8.0/_static/documentation_options.js
+++ v0.8.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.0.dev1579+g05797c41',
+    VERSION: '0.9.0.dev1581+g2c8d4498',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
