04cfbc7820f7c6847fe847aedae30fead18ca1c0
skoudoro <skab12@gmail.com>
2023-06-12 15:14:44 +0000

    Deploying to gh-pages from @ fury-gl/fury@bbdae8cace2d4619be0c2204f85657b4fbaff9dd 🚀

--- v0.9.x/_static/documentation_options.js
+++ v0.9.x/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.0.dev12+g86ab182c',
+    VERSION: '0.10.0.dev15+gbbdae8ca',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
