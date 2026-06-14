2676e52a12b9366b6772243067a7b7e9287f44c1
maharshi-gor <gor.maharshi@gmail.com>
2026-06-14 12:07:54 +0000

    Deploying to gh-pages from @ fury-gl/fury@cb01494921247206215a7ce6649a296126a08041 🚀

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.1.0.dev107+g58dff47f8',
+    VERSION: '2.1.0.dev112+gcb0149492',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
